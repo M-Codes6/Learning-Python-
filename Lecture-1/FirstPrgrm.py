e34a2a80668ded25243af3f37d638aec62628d58
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-17 22:19:47 +0530

    Initial commit: Python basics practice

---
+++ Lecture-1/FirstPrgrm.py
@@ -0,0 +1,89 @@
+print("Hello World")
+print("My self Muzamil")
+
+
+print("Hello World","My self Muzamil")
+
+name = "Muzamil"
+age = 21
+
+print(type(name))
+print(type(age))
+
+
+# SUM OF WO NUMBERS 
+
+a = 20
+b = 40
+sum = a + b
+print(sum)
+
+
+# ARTHEMATIC OPERATORS
+
+a = 10
+b = 20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #remainder
+print(a**b) #power
+
+
+# RELATIIONAL OPERATORS
+
+a = 50
+b = 20
+
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
+
+# ASSINGMENT OPERATORS
+
+num = 10
+num += 20
+print(num)
+
+
+# LOGICAL OPERATORS
+a = 50 
+b = 30
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = True
+print("AND operator", val1 and val2)
+print("OR operator", val1 or val2)
+
+
+# TYPE CONVERSION (automatically convert the data type)
+
+a = 5
+b = 6.5
+sum = a + b
+print (sum)
+
+# TYPE CASTING (manually convert the data type)
+
+a = int("5")
+b = 6.5
+
+sum = a + b
+print(sum)
+
+
+# INPUT IN PYTHON
+
+input("Enter your email ")
+input ("Enter your password ")
+
+name = input("Enter your name ")
+print("Welcome", name)
---
+++ practiceQ.py
@@ -0,0 +1,40 @@
+
+
+# Q1 : write a program to input 2 numbers and print their sum ?
+
+input1 = int(input("Enter first number: "))
+input2 = int(input("Enter second number: "))
+
+sum = input1 + input2
+
+print("Sum of two numbers is: ", sum)
+
+
+
+# Q2 : write a program to input side of a square and print its area ?
+
+a = int(input("Enter side of a square: "))
+area = a**2
+
+print("Area of square is: ", area)
+
+
+
+# Q3 : write a program to input 2 floating point numbers and print their average ?
+
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+
+average = ( a + b) / 2
+
+print("Average of two numbers is: ", average)
+
+
+
+# Q4 : write a program to input 2 int numbers , a and b . 
+#      Print True if a is greater or equal to b , if not print False ?
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a >= b)
