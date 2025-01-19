0144da3b94c83f2690d58c2082fe1ca1042bb051
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-19 14:05:16 +0530

    Practicing Questions on List MEthods

--- Lecture-3/PracticeQuestions.py
+++ Lecture-3/PracticeQuestions.py
@@ -36,3 +36,33 @@ print(grades.count("A"))
 list = list(grades)
 list.sort()
 print(list)
+
+
+# Q4 : Write a program to create a list of 5 elements and then remove the 3rd element from the list ?
+
+list = [1, 2, 3, 4, 5]
+list.pop(3)
+print(list)
+
+# Q5 : Write a program to insert a new element at the 2nd index of the list ?
+
+list = [1, 2, 3, 4, 5]
+list.insert(2, 6)
+print(list)
+
+# Q6 : Write a program to create a list of 5 elements and then sort the list in ascending order ?
+
+list = [5, 4, 3, 2, 1]
+list.sort()
+print(list)
+
+# Q7 : Write a program to create a list of 5 elements and then reverse the list ?
+
+list = [1, 2, 3, 4, 5]
+list.reverse()
+print(list)
+
+# Q8 : Write a program to create a list of 5 elements and then find the index of the element "3" ?
+
+list = [1, 2, 3, 4, 5]
+print(list.index(3))
