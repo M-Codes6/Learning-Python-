5eec1c932b7cc12a4fa8c91b0c6127270e19d0ee
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-20 12:58:19 +0530

    Dictionary And Sets

---
+++ Lecture-4/PracticeQuestions.py
@@ -0,0 +1,13 @@
+
+
+# Q1: Store following word meanings in a dictionary?
+# table: "a piece of furniture","list of facts and figures"
+# cat : "a cmall animal"
+
+word_meanings = {
+    "table" : {"a piece of furniture", "list of facts and figures"},
+    "cat" : "a small animal"
+}
+
+print(word_meanings)
+print(word_meanings["table"])
