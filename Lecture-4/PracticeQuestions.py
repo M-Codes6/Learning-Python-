bf44dbf76f45a1fd50857dc9c3c7685de2c16681
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-20 13:22:30 +0530

    Question 2 Solved

--- Lecture-4/PracticeQuestions.py
+++ Lecture-4/PracticeQuestions.py
@@ -5,9 +5,17 @@
 # cat : "a cmall animal"
 
 word_meanings = {
-    "table" : {"a piece of furniture", "list of facts and figures"},
+    "table" : ["a piece of furniture", "list of facts and figures"],
     "cat" : "a small animal"
 }
 
 print(word_meanings)
 print(word_meanings["table"])
+
+# Q2: You are given a list of subjects for students. Assume 1 class room is required for 1 sbuject.How many class rrooms are needed by all the students?
+# "python", "java", "c++", "c", "python", "javascript", "java", "c++"
+
+subjects = ["python", "java", "c++", "c", "python", "javascript", "java", "c++"]
+class_rooms = set(subjects)
+print(class_rooms)
+print(len(class_rooms))
