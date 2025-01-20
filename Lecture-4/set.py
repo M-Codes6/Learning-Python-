9124c98481e90ee3db0f1eefedf0f5ab439581e1
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-20 12:42:49 +0530

    Dictionary and Sets

--- Lecture-4/set.py
+++ Lecture-4/set.py
@@ -5,3 +5,20 @@ num = {1, 2, 3, 4}
 
 print(num)
 print(type(num))
+
+
+# SET METHODS
+
+num = {1, 2, 3, 4}
+
+num.add(5) # adds an element to the set
+print(num)
+
+num.remove(3) # removes an element from the set
+print(num)
+
+num.clear() # removes all the elements from the set
+print(num)
+
+collection = {"apple", "banana", "cherry"}
+print(collection.pop()) # removes the last element from the set
