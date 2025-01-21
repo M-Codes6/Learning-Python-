7dc19519e3d626e87b9581b3d125c44b9d0216d4
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-21 09:42:02 +0530

    While Loop

---
+++ Lecture-5/Loops.py
@@ -0,0 +1,33 @@
+
+# LOOPS IN PYTHON 
+
+# loops are used to repeate instructions
+
+# WHILE LOOP 
+
+count = 1
+
+while count <= 5 :
+    print("Hello")
+    count += 1
+
+print(count)
+
+
+i = 1 
+
+while i <= 5 :
+    print("buddy")
+
+    i += 1
+
+# We can also print itterators for example 
+
+i = 1
+
+while i <= 10 :
+
+    print("Heloo World", i)
+
+    i += 1
+
