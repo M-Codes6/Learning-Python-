6166bf017b144bf1d4367bf1fc2cd91986b2aa85
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-21 11:54:03 +0530

    Loops

--- Lecture-5/Loops.py
+++ Lecture-5/Loops.py
@@ -31,3 +31,43 @@ while i <= 10 :
 
     i += 1
 
+
+
+# Break And Continue 
+
+# Break 
+
+i = 0
+
+while i <= 5 :
+    print(i)
+    
+    if ( i == 3) :
+        break
+    i += 1
+
+print (" End of Loop")
+
+
+# Continue
+
+
+i = 0
+
+while i <= 5 :
+    
+    if ( i == 3) :
+        i += 1
+        continue
+   
+    print(i)
+    i += 1
+
+
+
+# For Loops 
+ 
+list = [1, 2,  3, 4, 5]
+
+for val in list :
+    print(val)
