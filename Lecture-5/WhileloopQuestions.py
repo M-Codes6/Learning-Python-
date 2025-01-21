fafc87d2b6ee3a2581b6a0389803aff8417e079f
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-21 10:32:35 +0530

    Solved Questions

--- Lecture-5/WhileloopQuestions.py
+++ Lecture-5/WhileloopQuestions.py
@@ -18,3 +18,46 @@ while num >= 1 :
     print(num)
 
     num -= 1
+
+
+
+
+# Q3: Print a multiplication table of a umber n with user input
+
+n = int(input("Enter number:"))
+i = 1 
+while i <= 10 :
+    print(n * i)
+
+    i += 1
+
+
+
+
+# Q4 : Print the elements of the list using a Loop 
+# [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+idx = 0
+
+while idx < len(nums) :
+    print(nums[idx])
+
+    idx += 1
+
+
+# Q5: Search for a number X in this tuple
+# (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+x = 49
+idx = 0 
+
+while idx < len(nums) :
+    if (nums[idx] == x) :
+
+        print("Found at idx:", x)
+
+    idx += 1
