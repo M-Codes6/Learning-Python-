532584693cf6115a2d08e059e28312f51163c84b
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-21 09:50:04 +0530

    Q! and Q2 solved

---
+++ Lecture-5/WhileloopQuestions.py
@@ -0,0 +1,20 @@
+
+
+# Q1 : Print numbers from 1 to 100
+
+num = 1
+
+while num <= 100 :
+    print(num)
+    num += 1
+
+
+
+# Q2 : Print numbers from 100 to 1
+
+num = 100
+
+while num >= 1 :
+    print(num)
+
+    num -= 1
