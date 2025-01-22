f443118345a2e12cdce475be07ee78371c57b97c
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-22 15:12:47 +0530

    Questions Solved

---
+++ Lecture-6/RecurssioonQuestions.py
@@ -0,0 +1,31 @@
+
+
+# Q1: Write a recursive function to calculate the sum of first n natural numbers ?
+
+def calc_sum(n):
+    if (n == 0):
+        return 0
+    return calc_sum(n-1) + n
+
+sum = calc_sum(5)
+print(sum)
+
+
+
+
+
+# Q2: Write a recursie function to print all elements in a list (Hint: use list and index as a parameter)
+
+
+def print_el(list, idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    print_el(list, idx+1)
+
+fruits = ["mango", "apple", "banana", "orange"]
+
+print_el(fruits)
+
+
+
