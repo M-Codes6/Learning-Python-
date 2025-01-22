6f033d3485e0239d871b58c257b82cbfea6ea191
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-22 09:28:11 +0530

    intial commit

---
+++ Lecture-6/function.py
@@ -0,0 +1,23 @@
+
+# Funtions in Python 
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+def sum(a, b) :
+
+    sum = a + b
+
+    return(sum)
+
+print(sum(a, b))
+
+# another eg
+
+def cal_sum (a, b) :
+
+    sum = a + b
+
+    return sum
+
+print(sum(2, 3))
