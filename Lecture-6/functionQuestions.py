38f9e6b3a0fada95a6420981586eb2d199ac5b23
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-22 10:03:02 +0530

    Q1 Solved

---
+++ Lecture-6/functionQuestions.py
@@ -0,0 +1,12 @@
+
+
+# Q1 : Write a function to print the length of a list. ( List is the parameter)
+
+nums = [2, 3, 4, 6, 7, 8]
+cities = ["Sopore", "Baramulla", "Srinagar", "Handwara"]
+
+def print_length(list) :
+    print(len(list))
+
+print(len(nums))
+print(len(cities))
