66a9ed480e5fdb5bb41b3c272c56fd5e23033ef0
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-22 09:49:44 +0530

    Functions

--- Lecture-6/function.py
+++ Lecture-6/function.py
@@ -14,10 +14,37 @@ print(sum(a, b))
 
 # another eg
 
-def cal_sum (a, b) :
+def cal_sum (a, b) :   
 
     sum = a + b
 
-    return sum
+    return sum 
 
 print(sum(2, 3))
+
+
+
+# Write a function to calculate average of 3 numbers
+
+def cal_avg(a, b, c) :
+
+    sum = a + b + c
+
+    avg = sum / 3
+
+    return(avg)
+
+print(cal_avg(2, 4, 3))
+
+
+
+
+
+# Function Types 
+
+# Built-in : print(), len(), type(), range() etc..
+# User-defined : not built-in , written by users
+
+
+# Default parameters : Assigning a default value to parameters which is used when no argument is passed
+
