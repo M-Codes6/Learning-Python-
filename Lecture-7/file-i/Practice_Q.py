e22eb8bcb72e2713a026af2e2ccb17920e27e14b
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-24 19:28:54 +0530

    File Input/Output

--- Lecture-7/file-i/Practice_Q.py
+++ Lecture-7/file-i/Practice_Q.py
@@ -29,12 +29,54 @@ with open("practice.txt", "w") as f:
 
 # Q3: Search if the word "learning" exists in the file or not 
 
-word = "learning"
 
-with open("practice.txt","r") as f:
+def check_for_word() :
+    word = "learning"
+    with open("practice.txt","r") as f:
+        data = f.read()
+
+        if(word in data):
+            print("Found")
+        else:
+            print("Not Found")
+
+check_for_word()
+
+
+    
+
+
+# Q4: WAF to find in which line of the file does the word "learning" occur  first. Print -1 if word not found
+
+def check_for_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt","r") as f:
+        while data :
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+                return
+            line_no += 1
+    return -1
+
+print(check_for_line())  
+
+
+
+
+
+
+# Q5: From a file containing numbers seperated by comma, print the count of even numbers 
+
+count = 0
+with open("practice-2.txt", "r") as f:
     data = f.read()
+    
+    nums = data.split(",")
+    for val in nums:
+        if(int(val) % 2 == 0) :
+            count += 1
 
-    if(data.find(word) != -1):
-       print("Found")
-    else:
-        print("Not Found")
+print(count)
