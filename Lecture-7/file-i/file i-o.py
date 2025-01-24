fc67410cb34d992ffdb64fcc302aee69beb915b7
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-24 17:45:06 +0530

    File i/o

--- Lecture-7/file-i/file i-o.py
+++ Lecture-7/file-i/file i-o.py
@@ -1,3 +1,17 @@
 
 
 # File i/o in python ...... i/o ( input / output)
+
+
+# Existing building functions to open, read and write a file 
+
+
+# OPen, Read and Close file
+
+f = open("file_name", "mode")   # file_name = sample.txt, demo.docx ... mode = r : read mode , w : write mode
+
+data = f.read()
+
+f.close()
+
+
