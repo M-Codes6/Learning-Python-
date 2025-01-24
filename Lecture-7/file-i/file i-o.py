cea095f465f31436441e7fcb2a27927f0148d499
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-24 17:59:09 +0530

    File I/O

--- Lecture-7/file-i/file i-o.py
+++ Lecture-7/file-i/file i-o.py
@@ -8,10 +8,27 @@
 
 # OPen, Read and Close file
 
-f = open("file_name", "mode")   # file_name = sample.txt, demo.docx ... mode = r : read mode , w : write mode
+'''f = open("file_name", "mode")   # file_name = sample.txt, demo.docx ... mode = r : read mode , w : write mode
+data = f.read()
+f.close() '''
+
+f = open("Lecture-7/file-i/demo.txt","r")
 
 data = f.read()
+print(data)
 
-f.close()
 
 
+# Reading a File 
+'''
+data = f.read()      # reads entire file
+data = f.readline()   # reads onne line at a time '''
+
+data = f.read()
+print(data)
+
+data = f.readline()
+print(data)
+
+data = f.readlines()
+print(data)
