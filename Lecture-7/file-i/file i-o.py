97d17502046267683f1e549c1b2696a48aa90120
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-24 18:22:02 +0530

    File i/o

--- Lecture-7/file-i/file i-o.py
+++ Lecture-7/file-i/file i-o.py
@@ -32,3 +32,52 @@ print(data)
 
 data = f.readlines()
 print(data)
+
+
+
+
+# Writing to a file 
+
+'''
+f = open("demo.txt","w")
+f.write("this is a new line)  #overwrites the entiree file 
+
+f = open("demo.txt","a")
+f.write("the this is next line) #adds to the file
+'''
+
+f = open("Lecture-7/file-i/demo.txt", "w")
+f.write("I am from kashmir :")
+f.close()
+
+
+f = open("Lecture-7/file-i/demo.txt", "a")
+f.write(" Sopore, Baramulla")
+f.close()
+
+
+
+# With Syntax
+
+'''
+with open("demo.txt", "mode") as f:
+        data = f.read()
+'''
+
+with open("Lecture-7/file-i/demo.txt","r") as f:
+    data = f.read()
+
+    print(data)
+
+
+# Deleting A File 
+
+'''
+using the os module
+# os module is a code library
+import os   
+os.remove(file name)
+'''
+
+import os 
+os.remove("Lecture-7/file-i/demo.txt")
