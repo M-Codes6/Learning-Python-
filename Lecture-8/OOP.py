080ee3955ece656f51e636c450d631d650fe3c6b
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-25 08:06:15 +0530

    oop

--- Lecture-8/OOP.py
+++ Lecture-8/OOP.py
@@ -25,3 +25,46 @@ car1 = Car
 print(car1.color)
 print(car1.mode)
 
+
+# __init__ function
+
+''' 
+Constructor : All classes have a function called __init__(), which is always executed when the
+              object is being initiated
+
+
+# Creating Class
+
+class Student:
+def __init__(self, fullname):
+        self.name = fullname
+
+
+# creating object
+
+s1 = Student()
+print(s1.name)
+
+
+The self parameter is a refrence to the current instance of the class, and is used 
+to access variables that belongs to the class.
+
+'''
+
+
+class Student:
+    def __init__(self, fullname):
+        self.name = fullname
+
+s1 = Student("M-Codes6")
+print(s1.name)
+
+
+class Car:
+    def __init__(self, newcar):
+        self.name = newcar
+    
+car1 = Car("Punch")
+car2 = Car("Curv")
+print(car1.name)
+print(car2.name)
