672b25e93eed6983a48e80f2edf1f2d1b2bdfef9
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-25 06:16:31 +0530

    OOP

---
+++ Lecture-8/OOP.py
@@ -0,0 +1,27 @@
+
+# OOP : to map with real world scenarios we started using objects in code
+
+# Class and Object 
+
+# Class : it is a blueprint for creating objects .
+
+'''e.g'''
+
+    # creating class
+class Student :
+    name = "Muzamil"
+
+    #creating object (instance)
+
+s1 = Student
+print(s1.name)
+
+
+class Car :
+    color ="Black"
+    mode = "Manuall"
+
+car1 = Car
+print(car1.color)
+print(car1.mode)
+
