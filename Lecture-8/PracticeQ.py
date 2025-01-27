c4e75f2694021d7a8ff2d3df8f7235c5894d40e4
Muzamil Mushtaq <M-Codes6@users.noreply.github.com>
2025-01-27 10:14:11 +0530

    OOPs

--- Lecture-8/PracticeQ.py
+++ Lecture-8/PracticeQ.py
@@ -45,4 +45,61 @@ acc1.credit(70000)
 print(acc1.get_balance())
 
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Account:
+    def __init__(self, bal, acc_no):
+        self.balance = bal  
+        self.acc_no = acc_no  
+    
+   def debit(self, amount):
+      self.balance -= amount  
+      print(f"Your a/c no. {self.acc_no} is debited for Rs. {amount:,}")
+      print(f"A/C Bal is INR {self.get_balance():,}")
+    
+    def credit(self, amount):
+        self.balance += amount  
+        print(f"A/C {self.acc_no} credited by INR {amount:,}")
+        print(f"A/C Bal is INR {self.get_balance():,}")
+    
+    def get_balance(self):
+        return self.balance  
+
+acc1 = Account(10_00_00_00_000, "xxxxx456")  
+
+acc1.debit(50_00_000)  
+acc1.credit(10_00_00_000)  
+print(f"Final Balance: Rs. {acc1.get_balance():,}")
+
+
+
+
+
+class Account:
+    def __init__(self, bal, acc_no):
+        self.balance = bal  
+        self.acc_no = acc_no
+
+    def debit(self, amount):
+        self.balance -= amount  
+        print(f"Your a/c no. {self.acc_no} is debited for Rs.{amount:,}")
+        print(f"A/C Bal is INR {self.get_balance():,}")
+    
+    def get_balance(self):
+        return self.balance  
+    
+acc1 = Account(10_00_00_00_000, "xxxxx456") 
+acc1.debit(int(input("Enter amount to be debited"))) 
+print(f"Final Balance: Rs. {acc1.get_balance():,}")
+
